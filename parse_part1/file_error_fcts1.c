39f3c1319c7e7882d1013ebe52e31b96c5f5c90d
oessayeg <offissayegh001@gmail.com>
2022-06-02 21:59:24 +0100

    norme

--- parse_part1/file_error_fcts1.c
+++ parse_part1/file_error_fcts1.c
@@ -6,69 +6,69 @@
 /*   By: oessayeg <oessayeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:00:55 by oessayeg          #+#    #+#             */
-/*   Updated: 2022/05/20 19:07:56 by oessayeg         ###   ########.fr       */
+/*   Updated: 2022/06/02 21:49:49 by oessayeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "file_parse.h"
 
-void check_args(int argc)
+void	check_args(int argc)
 {
-    if (argc == 1)
-       printf("Error\nThere must be 2 arguments.\n");
-    else if (argc > 2)
-        printf("Error\nToo many arguments found.\n");
-    if (argc == 1 || argc > 2)
-        exit(EXIT_FAILURE);
+	if (argc == 1)
+		printf("Error\nThere must be 2 arguments.\n");
+	else if (argc > 2)
+		printf("Error\nToo many arguments found.\n");
+	if (argc == 1 || argc > 2)
+		exit(EXIT_FAILURE);
 }
 
-void check_file_name(char *file)
+void	check_file_name(char *file)
 {
-    int i;
-    
-    i = ft_strlen(file) - 1;
-    if (file[0] == '.')
-    {
-        printf("Error\nHidden file detected.\n");
-        exit(EXIT_FAILURE);
-    }
-    while (i >= 0)
-    {
-        if (file[i] == '.')
-        {
-            if (ft_strncmp(&file[i], ".cub", 4) == 0 && file[i + 4] == '\0')
-                return ;
-        }        
-        i--; 
-    }
-    printf("Error\n");
-    printf("Map not ending with '.cub' extension.\n");
-    exit(EXIT_FAILURE);
+	int	i;
+
+	i = ft_strlen(file) - 1;
+	if (file[0] == '.')
+	{
+		printf("Error\nHidden file detected.\n");
+		exit(EXIT_FAILURE);
+	}
+	while (i >= 0)
+	{
+		if (file[i] == '.')
+		{
+			if (ft_strncmp(&file[i], ".cub", 4) == 0 && file[i + 4] == '\0')
+				return ;
+		}
+		i--;
+	}
+	printf("Error\n");
+	printf("Map not ending with '.cub' extension.\n");
+	exit(EXIT_FAILURE);
 }
 
-void    check_file_permissions(char *file)
+void	check_file_permissions(char *file)
 {
-    int fd;
+	int	fd;
 
-    fd = open(file, O_RDONLY);
-    if (fd < 0)
-    {
-        printf("Error\n");
-        perror(file);
-        exit(EXIT_FAILURE);
-    }
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error\n");
+		perror(file);
+		exit(EXIT_FAILURE);
+	}
 }
 
-char **check_file_args(int argc, char **argv)
+char	**check_file_args(int argc, char **argv)
 {
-    char **ptr;
+	char	**ptr;
 
-    ptr = NULL;
-    check_args(argc);
-    check_file_name(argv[1]);
-    check_file_permissions(argv[1]);
-    ptr = file_to_2darr(argv[1]);
-    if (ptr == NULL || ft_lenp(ptr) < 9)
-        exit_msg(ptr);
-    return (ptr);   
+	ptr = NULL;
+	check_args(argc);
+	check_file_name(argv[1]);
+	check_file_permissions(argv[1]);
+	ptr = file_to_2darr(argv[1]);
+	if (ptr == NULL || ft_lenp(ptr) < 9)
+		exit_msg(ptr);
+	return (ptr);
 }
--- parse_part1/file_error_fcts2.c
+++ parse_part1/file_error_fcts2.c
@@ -6,115 +6,115 @@
 /*   By: oessayeg <oessayeg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 16:51:33 by oessayeg          #+#    #+#             */
-/*   Updated: 2022/05/21 12:48:10 by oessayeg         ###   ########.fr       */
+/*   Updated: 2022/06/02 21:59:06 by oessayeg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "file_parse.h"
 
-void skip_spaces(char *string, int *i)
+void	skip_spaces(char *string, int *i)
 {
-    while (string[*i] == ' ')
-        (*i)++;
+	while (string[*i] == ' ')
+		(*i)++;
 }
 
-int is_good_num(char *string, int *i, int option, char **s)
+int	is_good_num(char *string, int *i, int option, char **s)
 {
-    int value;
+	int	value;
 
-    value = 0;
-    if (option == 1)
-    {
-        while (string[*i] != '\0')
-        {
-            if (!(string[*i] >= '0' && string[*i] <= '9'))
-            {
-                skip_spaces(string, i);
-                if (string[*i] == '\n' && string[*i + 1] == '\0')
-                    break;
-                else
-                    return (0);
-            }
-            value = value * 10 + (string[*i] - 48);
-            (*i)++;
-        }
-    }
-    else
-    {
-        while(string[*i] != '\0')
-        {
-            if (!(string[*i] >= '0' && string[*i] <= '9') && (string[*i] != ','))
-                return (0);
-            if (string[*i] == ',' && string[*i + 1] != '\0')
-            {
-                (*i)++;
-                if (string[*i] == ',')
-                    exit_msg(s);
-                break;
-            }
-            value = value * 10 + (string[*i] - 48);
-            (*i)++;
-        }
-    }
-    if (value >= 0 && value <= 255)
-        return (1);
-    return (0);
+	value = 0;
+	if (option == 1)
+	{
+		while (string[*i] != '\0')
+		{
+			if (!(string[*i] >= '0' && string[*i] <= '9'))
+			{
+				skip_spaces(string, i);
+				if (string[*i] == '\n' && string[*i + 1] == '\0')
+					break;
+				else
+					return (0);
+			}
+			value = value * 10 + (string[*i] - 48);
+			(*i)++;
+		}
+	}
+	else
+	{
+		while(string[*i] != '\0')
+		{
+			if (!(string[*i] >= '0' && string[*i] <= '9') && (string[*i] != ','))
+				return (0);
+			if (string[*i] == ',' && string[*i + 1] != '\0')
+			{
+				(*i)++;
+				if (string[*i] == ',')
+					exit_msg(s);
+				break;
+			}
+			value = value * 10 + (string[*i] - 48);
+			(*i)++;
+		}
+	}
+	if (value >= 0 && value <= 255)
+		return (1);
+	return (0);
 }
 
-int check_ceil_floor(char *string, char **s)
+int	check_ceil_floor(char *string, char **s)
 {
-    int     i;
-    int     value;
-    char    c;
+    int		i;
+    int		value;
+    char	c;
 
-    c = 'C';
-    value = 0;
-    i = 0;
-    skip_spaces(string,  &i);
-    if (string[i] != 'F' && string[i] != 'C' && (!((string[i] == 'N' && string[i + 1] == 'O')
-        || (string[i] == 'S' && string[i + 1] == 'O')
-        || (string[i] == 'W' && string[i + 1] == 'E')
-        || (string[i] == 'E' && string[i + 1] == 'A'))))
-        exit_msg(s);
-    if (string[i] == 'F')
-        c = 'F';
-    else if (string[i] != 'C')
-        return (0);
-    i++;
-    skip_spaces(string, &i);
-    if (string[i] == ',')
-        exit_msg(s);
-    if (is_good_num(string, &i, 0, s) && is_good_num(string, &i, 0, s)
-        && is_good_num(string, &i, 1, s))
-        {
-            if (c == 'F')
-                return (1);
-            else if (c == 'C')
-                return (2);
-        }
-    exit_msg(s);
-    return (0);
+	c = 'C';
+	value = 0;
+	i = 0;
+	skip_spaces(string,  &i);
+	if (string[i] != 'F' && string[i] != 'C' && (!((string[i] == 'N' && string[i + 1] == 'O')
+			|| (string[i] == 'S' && string[i + 1] == 'O')
+			|| (string[i] == 'W' && string[i + 1] == 'E')
+			|| (string[i] == 'E' && string[i + 1] == 'A'))))
+		exit_msg(s);
+	if (string[i] == 'F')
+		c = 'F';
+	else if (string[i] != 'C')
+		return (0);
+	i++;
+	skip_spaces(string, &i);
+	if (string[i] == ',')
+		exit_msg(s);
+	if (is_good_num(string, &i, 0, s) && is_good_num(string, &i, 0, s)
+			&& is_good_num(string, &i, 1, s))
+	{
+		if (c == 'F')
+			return (1);
+		else if (c == 'C')
+			return (2);
+	}
+	exit_msg(s);
+	return (0);
 }
 
-void init_info_check(t_mapCheck *info)
+void	init_info_check(t_mapCheck *info)
 {
-    info->ceiling_color = 0;
-    info->east_tex = 0;
-    info->floor_color = 0;
-    info->north_tex = 0;
-    info->south_tex = 0;
-    info->west_tex = 0;
+	info->ceiling_color = 0;
+	info->east_tex = 0;
+	info->floor_color = 0;
+	info->north_tex = 0;
+	info->south_tex = 0;
+	info->west_tex = 0;
 }
 
-char **check_infos_in(char **file)
+char	**check_infos_in(char **file)
 {
-    int         i;
-    t_mapCheck  info_check;
+    int			i;
+    t_mapCheck	info_check;
 
-    i = 0;
-    init_info_check(&info_check);
-    while (file[i] != NULL)
-    {
+	i = 0;
+	init_info_check(&info_check);
+	while (file[i] != NULL)
+	{
         if (file[i][0] != '\n')
         {
             if (check_ceil_floor(file[i], file) == 1)
